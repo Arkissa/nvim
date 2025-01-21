f8eeec6390ed452733b54afcf2736e657a146403
Arkissa <mrarkssac@gmail.com>
2025-01-21 19:13:05 +0800

    refactor: ftplugin using opt_local

--- after/ftplugin/haskell/options.lua
+++ after/ftplugin/haskell/options.lua
@@ -1,4 +1,4 @@
-local opt = vim.opt
+local opt = vim.opt_local
 opt.listchars:append { lead = "∙" }
 opt.expandtab = true
 opt.tabstop = 2
--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,3 +1,3 @@
-local opt = vim.opt
+local opt = vim.opt_local
 opt.tabstop = 2
 opt.shiftwidth = 2
