d9e6bbc9802febcdda93cb4b1aef1744a850d37c
Arkissa <mrarkssac@gmail.com>
2025-02-21 13:21:09 +0800

    delete: startup provider and go tab style

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 vim.loader.enable()
 
+vim.g.loaded_ruby_provider = false
+vim.g.loaded_perl_provider = false
+vim.g.loaded_node_provider = false
+vim.g.loaded_python3_provider = false
+
 _G.augroup = vim.api.nvim_create_augroup
 _G.autocmd = vim.api.nvim_create_autocmd
 
