71648b3ff0cc793a22bb737a78a592332ea0c808
Arkkissa <mrarkssac@gmail.com>
2025-04-16 17:18:12 +0800

    update: misc

--- init.lua
+++ init.lua
@@ -8,7 +8,4 @@ vim.g.loaded_python3_provider = false
 _G.augroup = vim.api.nvim_create_augroup
 _G.autocmd = vim.api.nvim_create_autocmd
 
-vim.g.markdown_syntax_conceal = 2
-vim.g.kastemds_path = "~/kastemd"
-
 vim.cmd.packadd "cfilter"
