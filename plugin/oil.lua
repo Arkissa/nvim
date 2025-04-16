ba1d14ee424397c7b60675476725933f35bf84f4
Arkkissa <mrarkssac@gmail.com>
2025-04-16 17:14:48 +0800

    update: misc

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -6,7 +6,26 @@ require "oil".setup {
 	},
 	keymaps = {
 		["<C-s>"] = { "actions.select", opts = { vertical = true, split = "belowright" } },
+		["<C-k>"] = "actions.open_terminal"
+	},
+	float = {
+		-- Padding around the floating window
+		padding = 2,
+		max_width = 100,
+		max_height = 20,
+		border = "rounded",
+		win_options = {
+			winblend = 0,
+		},
+		-- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+		get_win_title = nil,
+		-- preview_split: Split direction: "auto", "left", "right", "above", "below".
+		preview_split = "auto",
+		-- This is the config that will be passed to nvim_open_win.
+		-- Change values here to customize the layout
+		override = function(conf)
+			return conf
+		end,
 	},
 }
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-vim.keymap.set("n", "<LEADER>-", "<CMD>vsplit | Oil<CR>", { desc = "Open parent vsplit directory" })
+vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
--- plugin/treesitter/treesitter.lua
+++ plugin/treesitter/treesitter.lua
@@ -36,7 +36,7 @@ require 'nvim-treesitter.configs'.setup {
 
 	highlight = {
 		enable = true,
-		disable = { 'markdown' },
+		-- disable = { 'markdown' },
 		-- additional_vim_regex_highlighting = true,
 	},
 	incremental_selection = {
