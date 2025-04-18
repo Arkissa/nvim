313502eb650e3404ae76b299c71e32705d964ab9
Arkkissa <mrarkssac@gmail.com>
2025-04-18 14:21:55 +0800

    feat: set select buffer filetype to markdown

--- plugin/ui/select.lua
+++ plugin/ui/select.lua
@@ -2,7 +2,7 @@ local select = augroup("select", {})
 local api = vim.api
 local bo = vim.bo
 
----@return integer
+---@return {winnr: integer, bufnr: integer}
 local function win_open(height, width, title)
 	local bufnr = api.nvim_create_buf(false, false)
 	local winnr = api.nvim_open_win(bufnr, true, {
@@ -18,12 +18,13 @@ local function win_open(height, width, title)
 	})
 
 	vim.wo[winnr].wrap = false
-	return bufnr
+	return {winnr = winnr, bufnr = bufnr}
 end
 
 local function set_lines(bufnr, lines)
 	bo[bufnr].buftype = "nofile"
 	bo[bufnr].bufhidden = "wipe"
+	bo[bufnr].filetype = "markdown"
 	api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
 	bo[bufnr].readonly = true
 	bo[bufnr].modifiable = false
@@ -37,46 +38,46 @@ vim.ui.select = function(items, opts, on_choice)
 	if vim.tbl_isempty(items) then
 		error("items must be non-empty.")
 	end
-	local abort = function()
-		on_choice(nil)
+
+	local choice = function(bufnr, item, idx)
+		api.nvim_buf_delete(bufnr, { force = true })
+		on_choice(item, idx)
 	end
 
 	local formatted = vim.tbl_map(opts.format_item, items)
 	local max_len_item = vim.fn.max(vim.iter(formatted)
 		:map(function(item)
 			return #item
-		end):totable())
+		end)
+		:totable())
 
-	local bufnr = win_open(#items, max_len_item, opts.prompt or "Select:")
+	local win = win_open(#items, max_len_item, opts.prompt or "Select:")
 
-	set_lines(bufnr, vim.iter(ipairs(formatted))
+	set_lines(win.bufnr, vim.iter(ipairs(formatted))
 		:map(function(i, v)
-			local padding = tonumber(#tostring(#formatted)) + 1
-			return ("(%%-%dd%%s"):format(padding):format(i, v)
+			local padding = #tostring(#formatted) + 2
+			return ("%%-%ds%%s"):format(padding):format(tostring(i)..'.', v)
 		end)
 		:totable())
 
 	autocmd({ "WinLeave", "BufLeave", "BufHidden" }, {
 		group = select,
-		buffer = bufnr,
+		buffer = win.bufnr,
 		callback = function(args)
-			api.nvim_buf_delete(args.buf, { force = true })
-			abort()
+			choice(args.buf)
 		end
 	})
 
-	vim.keymap.set('n', "q", function ()
-		api.nvim_buf_delete(bufnr, { force = true })
-		abort()
-	end, { noremap = true, buffer = bufnr })
-	vim.keymap.set('n', "<ESC>", function ()
-		api.nvim_buf_delete(bufnr, { force = true })
-		abort()
-	end, { noremap = true, buffer = bufnr })
+	vim.keymap.set('n', "q", function()
+		choice(win.bufnr)
+	end, { noremap = true, buffer = win.bufnr })
 
-	vim.keymap.set('n', "<CR>", function ()
-		local idx = api.nvim_win_get_cursor(bufnr)[1]
-		api.nvim_buf_delete(bufnr, { force = true })
-		on_choice(items[idx], idx)
-	end, { noremap = true, buffer = bufnr })
+	vim.keymap.set('n', "<ESC>", function()
+		choice(win.bufnr)
+	end, { noremap = true, buffer = win.bufnr })
+
+	vim.keymap.set('n', "<CR>", function()
+		local idx = api.nvim_win_get_cursor(win.winnr)[1]
+		choice(win.bufnr, items[idx], idx)
+	end, { noremap = true, buffer = win.bufnr })
 end
