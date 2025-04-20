2e1cd25d849d06c059919e65e15a2869a0a11ccb
Arkkissa <mrarkssac@gmail.com>
2025-04-20 20:19:43 +0800

    update: create command in a file.

---
+++ plugin/quickfix/command.lua
@@ -0,0 +1,52 @@
+local Finder = require "quickfix.find"
+vim.api.nvim_create_user_command(
+	"Vimgrep",
+	function(args)
+		vim.cmd[args.bang and "vimgrepadd" or "vimgrep"]({ args = { string.format([[/\v%s/gj]], args.args), "**/*" }, mods = { silent = true } })
+	end,
+	{
+		nargs = 1,
+		bang = true,
+		desc = [[A vimgrep, but default use \v very magic for regexp.]]
+	})
+
+vim.api.nvim_create_user_command(
+	"Vimwords",
+	function(args)
+		vim.cmd[args.bang and "vimgrepadd" or "vimgrep"]({ args = { string.format([[/\v<%s>/gj]], args.args), "**/*" }, mods = { silent = true } })
+	end, {
+		nargs = 1,
+		bang = true,
+		desc = [[A vimgrep pattern word, argument must be `iskeyword`, but default use \v very magic for regexp.]]
+	})
+
+vim.api.nvim_create_user_command(
+	"Lvimwords",
+	function(args)
+		vim.cmd[args.bang and "vimgrepadd" or "vimgrep"]({ args = { string.format([[/\v<%s>/gj]], args.args), "**/*" }, mods = { silent = true } })
+	end, {
+		nargs = 1,
+		bang = true,
+		desc = [[A vimgrep pattern word, argument must be `iskeyword`, but default use \v very magic for regexp.]]
+	})
+
+vim.api.nvim_create_user_command(
+	"Lvimwords",
+	function(args)
+		vim.cmd[args.bang and "vimgrepadd" or "vimgrep"]({ args = { string.format([[/\v<%s>/gj]], args.args), "**/*" }, mods = { silent = true } })
+	end, {
+		nargs = 1,
+		bang = true,
+		desc = [[A vimgrep pattern word, argument must be `iskeyword`, but default use \v very magic for regexp.]]
+	})
+
+vim.api.nvim_create_user_command("Find",
+	function(args)
+		Finder():find(args.args)
+		vim.cmd.copen()
+	end,
+	{
+		nargs = 1,
+		desc = "Find file by name. using the external gnu find."
+	})
+
--- plugin/quickfix/quickfix.lua
+++ plugin/quickfix/quickfix.lua
@@ -1,18 +1,4 @@
 local qf = augroup("qf", { clear = false })
-local Finder = require "quickfix.find"
-local finder = Finder()
-
-vim.api.nvim_create_user_command("Grep", function (args)
-	vim.cmd[args.bang and "vimgrepadd" or "vimgrep"]({ args = { string.format("/%s/gj", args.args), "**/*" }, mods = { silent = true } })
-end, { nargs = 1, bang = true })
-
-vim.api.nvim_create_user_command("Words", function (args)
-	vim.cmd[args.bang and "vimgrepadd" or "vimgrep"]({ args = { string.format([[/\<%s\>/gj]], args.args), "**/*" }, mods = { silent = true } })
-end, { nargs = 1 })
-
-vim.api.nvim_create_user_command("Find", function(args)
-	finder:find(args.args)
-end, { nargs = 1})
 vim.o.quickfixtextfunc = "v:lua.require'quickfix.textfunc'.func"
 
 autocmd("QuickFixCmdPost", {
