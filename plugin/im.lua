5188184fa8a13702dc77113254200c494b1c6fd6
Arkkissa <mrarkssac@gmail.com>
2025-04-24 10:05:51 +0800

    feat: record last leaved Insert im-mode.

--- plugin/im.lua
+++ plugin/im.lua
@@ -2,6 +2,9 @@ if not vim.fn.executable("ibus") then
 	return
 end
 
+---@type string
+local last_mode = ""
+
 autocmd("ModeChanged", {
 	pattern = "i:n",
 	callback = function ()
@@ -10,8 +13,20 @@ autocmd("ModeChanged", {
 			return
 		end
 
-		if vim.trim(object.stdout) == "rime" then
+		local mode = vim.trim(object.stdout)
+
+		last_mode = mode
+		if  mode ~= "xkb:us::eng" then
 			vim.system({ "ibus", "engine", "xkb:us::eng" }):wait()
+		end
+	end
+})
+
+autocmd("ModeChanged", {
+	pattern = "n:i",
+	callback = function ()
+		if last_mode ~= "" then
+			vim.system({ "ibus", "engine", last_mode }):wait()
 		end
 	end
 })
