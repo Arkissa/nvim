cff54af1453071c587abe38e0f78773d2390c66a
Arkkissa <mrarkssac@gmail.com>
2025-04-28 15:12:38 +0800

    update: Grep instead of Cgrep.

--- after/plugin/grepprg.lua
+++ after/plugin/grepprg.lua
@@ -2,75 +2,42 @@ if not vim.fn.executable("cgrep") then
 	return
 end
 
-local Cgrep = require "quickfix.cgrep"
-vim.opt.grepformat = "%f:%l:%c:%m"
+local cgrep = {
+	"cgrep",
+	"-r",
+}
 
-vim.api.nvim_create_user_command("Cgrep", function(args)
-	vim.opt.grepprg = Cgrep()
-	vim.cmd.grep({ args = args.fargs, mods = { silent = true }, bang = not args.bang })
-end, {
-	nargs = '*',
-	bang = true,
-	complete = function()
-		return {
-			"-c",
-			"--code",
-			"-m",
-			"--comment",
-			"-l",
-			"--literal",
-			"--name",
-			"--identifier",
-			"--type",
-			"--native",
-			"--keyword",
-			"--number",
-			"--string",
-			"--op",
-			"-S",
-			"--semantic",
-			"--max-count=",
-			"-t",
-			"--type-filter=",
-			"-k",
-			"--kind-filter=",
-			"--force-type=",
-			"--type-list",
-			"-v",
-			"--invert-match",
-			" --multiline=",
-			"-r",
-			"--recursive",
-			"-T",
-			"--skip-test",
-			"--prune-dir=",
-			"-L",
-			"--follow",
-			"--show-match",
-			"--color",
-			"--no-color",
-			"-h",
-			"--no-filename",
-			"--no-numbers",
-			"--no-column",
-			"--count",
-			"--filename-only",
-			"--json",
-			"--vim",
-			"--editor",
-			"--fileline",
-			"-j",
-			"--threads=",
-			"--verbose=",
-			"--no-shallow",
-			"--palette",
-			"-?",
-			"--help",
-			"-V",
-			"--version",
-			"--numeric-version",
-		}
-	end
-})
+local default_exculd_dirs = {
+	".git/",
+	"__pycache__/",
+	"dist-newstyle/",
+	"node_modules/",
+}
 
-vim.cmd.Alias({ args = {"grep", "Cgrep"}})
+local prune_dir = vim.iter({default_exculd_dirs, vim.g.cgrep_exculd_dirs or {}})
+	:flatten()
+	:map(function(dir)
+		return "--prune-dir=" .. dir
+	end)
+	:totable()
+
+local kind_filter = vim.iter(vim.g.cgrep_kind_filter or {})
+	:map(function(dir)
+		return "--kind-fiters=" .. dir
+	end)
+	:totable()
+
+local type_filter = vim.iter(vim.g.cgrep_type_filter or {})
+	:map(function(dir)
+		return "--type-filter=" .. dir
+	end)
+	:totable()
+
+vim.opt.grepprg = vim.iter({ cgrep, prune_dir, kind_filter, type_filter }):flatten():join(' ') .. " $* ."
+vim.opt.grepformat = "%-G,%f:%l:%c:%m"
+
+if vim.fn.exists(":Grep") == 0 then
+	return
+end
+
+vim.cmd.Alias({ args = {"grep", "Grep"}})
---
+++ lua/grep.lua
@@ -0,0 +1,77 @@
+local M = {}
+M.__index = M
+
+local Quickfix = require "quickfix"
+
+---@param name string
+---@return any|nil
+local function get_option(name)
+	local ok, result = pcall(vim.api.nvim_get_option_value, name, {scope = "global"})
+	if not ok then
+		ok, result = pcall(vim.api.nvim_get_option_value, name, {scope = "local"})
+		if not ok then
+			return nil
+		end
+	end
+
+	if result == "" then
+		return nil
+	end
+
+	return result
+end
+
+---@param flags string[]
+---@param winnr integer?
+function M.Grep(flags, winnr)
+	local grepprg = get_option("grepprg")
+	if grepprg == nil then
+		return vim.notify("Not found grepprg option", vim.log.levels.ERROR)
+	end
+
+	local alternate_file_name = grepprg:match("#[^ ]*")
+
+	if alternate_file_name ~= nil then
+		grepprg = grepprg:gsub(alternate_file_name, vim.fn.expand(alternate_file_name))
+	end
+
+	local current_file_name = grepprg:match("%%:.*")
+	if current_file_name ~= nil then
+		grepprg = grepprg:gsub(current_file_name, vim.fn.expand(current_file_name))
+	end
+
+	grepprg = grepprg:gsub([[%$%*]], vim.iter(flags):join(" "))
+
+	local gfm = get_option("grepformat")
+	if gfm == nil then
+		return vim.notify("Not found grepformat option", vim.log.levels.ERROR)
+	end
+
+	local qf = Quickfix(winnr)
+	qf:setlist({}, 'r', {title = "Grep"})
+	vim.fn.jobstart(grepprg, {
+		stdout_buffered = false,
+		on_stdout = vim.schedule_wrap(function (_, lines, _)
+			if lines == nil or vim.tbl_isempty(lines) then
+				return
+			end
+
+			qf:setlist({}, 'a', { efm = gfm, lines = lines})
+		end),
+		on_exit = vim.schedule_wrap(function()
+			qf:window()
+		end)
+	})
+end
+
+function M.create_command()
+	vim.api.nvim_create_user_command("Grep", function (args)
+		M.Grep(args.fargs)
+	end, { nargs = "*", bang = true })
+
+	vim.api.nvim_create_user_command("LGrep", function (args)
+		M.Grep(args.fargs, vim.api.nvim_get_current_win())
+	end, { nargs = "*", bang = true })
+end
+
+return M
