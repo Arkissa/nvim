f273a1b8943c48781e5433584bedf41e631f995c
Arkkissa <mrarkssac@gmail.com>
2025-04-29 21:06:54 +0800

    update: more better go dev environment.

--- after/ftplugin/go/under_path.lua
+++ after/ftplugin/go/under_path.lua
@@ -1,9 +1,7 @@
-vim.opt_local.spell = true
-vim.cmd.compiler("go")
-
 local gopkg = vim.fs.joinpath(vim.fn.trim(vim.fn.system("go env GOPATH")), "pkg", "mod")
 local goroot = vim.fs.joinpath(vim.fn.trim(vim.fn.system("go env GOROOT")), "src")
 vim.g.under_path(function(path)
 	return vim.fs.relpath(goroot, path)
 		or vim.fs.relpath(gopkg, path)
 end)
+
