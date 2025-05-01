05e2a01e5741af2290ed97b13bea81aff1843035
Arkkissa <mrarkssac@gmail.com>
2025-05-01 22:45:56 +0800

    update: closed semanticTokens server_capabilities.

--- lsp/luals.lua
+++ lsp/luals.lua
@@ -1,7 +1,11 @@
+---@type vim.lsp.Config
 return {
 	cmd = { "lua-language-server" },
 	root_markers = { ".git", '.luarc.json', '.luarc.jsonc', "stylua.toml", ".stylua.toml" },
 	filetypes = { "lua" },
+	on_init = function(client)
+		client.server_capabilities.semanticTokensProvider = nil
+	end,
 	settings = {
 		Lua = {
 			runtime = { version = "LuaJIT" },
